fe0c15dd319be3d9cfb596238ac4c52063b5d79d
Upasana <upasanapaudyal@gmail.com>
2019-09-28 11:14:53 -0400

    throw an error

--- scoring.js
+++ scoring.js
@@ -48,6 +48,7 @@ module.exports.calculateScore = function (player) {
     case 'TE':
       return calculateReceivingScore(player)
     default:
-      return 0
+      throw ("Unknown player position (K)")
+
   }
 }
